d647511e4393706b364eff6245123e271511c191
Petersaad5 <152736305+Petersaad5@users.noreply.github.com>
2024-07-15 10:12:39 +0200

    Added home  filtering

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,12 +10,12 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type = "text" placeholder = "Filter by city">
-        <button class = "primary" type = "button ">Search</button>
+        <input type = "text" placeholder = "Filter by city" #filter><!--# used to give template variable that we can referance later  -->
+        <button class = "primary" type = "button" (click)="filterResults(filter.value)" >Search</button>
       </form>
     </section>
     <section class = "results">
-      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
@@ -24,10 +24,20 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService); // equivalent d'appel de constructeur
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingService.getAllHousingLocations().then((housingLocationList:HousingLocation[]) => {
       this.housingLocationList=housingLocationList; // then used to assign housing location list of the page to  what is returned by the function
+      this.filteredLocationList = this.housingLocationList;
     });
   }
+  filterResults(text: string){
+    if(!text) this.filteredLocationList = this.housingLocationList;
+      
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation=>housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
 }
